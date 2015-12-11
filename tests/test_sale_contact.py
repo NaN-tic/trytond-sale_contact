3554580f791bc2c074cbc8210c857140fa57b63d
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-11 23:04:45 +0100

    Migrate to version 3.8 (grafted from 1c7d704963c6ef5763039498a72378b49046f8d6)

--- tests/test_sale_contact.py
+++ tests/test_sale_contact.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_contact')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_contact')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class TestSaleContactCase(ModuleTestCase):
+    'Test Sale Contact module'
+    module = 'sale_contact'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            TestSaleContactCase))
     return suite
